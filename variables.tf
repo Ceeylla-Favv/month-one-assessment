35d7863c51209fcdb25875885ddb8c3c1bf0d346
Ceeylla-Favv <bertleypriscy@gmail.com>
2026-04-09 17:29:51 +0100

    feat(variables): declare input variables for region, instance types, key pair and IP

--- variables.tf
+++ variables.tf
@@ -3,3 +3,25 @@ variable "region" {
   type        = string
   default     = "eu-west-1"
 }
+
+variable "instance_type_web" {
+  description = "Instance type for web/bastion servers"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_type_db" {
+  description = "Instance type for DB server"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS key pair"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "IP address with /32 (e.g. 1.2.3.4/32)"
+  type        = string
+}
