c0bc7d45a4ff4e14dcae43eb9c4a15c80f6ae301
Ceeylla-Favv <bertleypriscy@gmail.com>
2026-04-14 15:30:03 +0100

    feat: add provider block and AMI data source

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
