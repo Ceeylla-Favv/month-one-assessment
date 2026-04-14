8393b24a9f688eec3f410f5001b41146c6941f4a
Ceeylla-Favv <bertleypriscy@gmail.com>
2026-04-14 15:51:36 +0100

    feat(nat): add internet gateway, NAT gateways and route tables
    Public route table sends traffic to IGW.
    Private route tables route outbound traffic through NAT gateways so private servers can download software without being exposed.

--- main.tf
+++ main.tf
@@ -59,3 +59,84 @@ resource "aws_subnet" "private_2" {
   availability_zone = "${var.region}b"
   tags              = { Name = "techcorp-private-subnet-2" }
 }
+
+#----Internet Gateway-----
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "techcorp-igw" }
+}
+
+
+#----Elastic IP for NAT Gateways------
+resource "aws_eip" "nat_1" {
+  domain = "vpc"
+  tags   = { Name = "techcorp-nat-eip-1" }
+}
+
+resource "aws_eip" "nat_2" {
+  domain = "vpc"
+  tags   = { Name = "techcorp-nat-eip-2" }
+}
+
+#-----NAT Gateways--------
+resource "aws_nat_gateway" "nat_1" {
+  allocation_id = aws_eip.nat_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.main]
+  tags          = { Name = "techcorp-nat-1" }
+}
+
+resource "aws_nat_gateway" "nat_2" {
+  allocation_id = aws_eip.nat_2.id
+  subnet_id     = aws_subnet.public_2.id
+  depends_on    = [aws_internet_gateway.main]
+  tags          = { Name = "techcorp-nat-2" }
+}
+
+#-----Route Table---------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = { Name = "techcorp-public-rt" }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_1.id
+  }
+  tags = { Name = "techcorp-private-rt-1" }
+}
+
+resource "aws_route_table" "private_2" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_2.id
+  }
+  tags = { Name = "techcorp-private-rt-2" }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_2.id
+}
