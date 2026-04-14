d9b12a27f61a789710e0c068a7d19385a10b7530
Ceeylla-Favv <bertleypriscy@gmail.com>
2026-04-14 15:41:23 +0100

    feat(subnets): add public and private subnets across two availability zones
    Two public subnets (10.0.1.0/24, 10.0.2.0/24) and two private subnets (10.0.3.0/24, 10.0.4.0/24) in eu-west-1a and eu-west-1b.

--- main.tf
+++ main.tf
@@ -28,3 +28,34 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   tags                 = { Name = "techcorp-vpc" }
 }
+
+#----Subnets-----
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "techcorp-public-subnet-1" }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "techcorp-public-subnet-2" }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.region}a"
+  tags              = { Name = "techcorp-private-subnet-1" }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.region}b"
+  tags              = { Name = "techcorp-private-subnet-2" }
+}
